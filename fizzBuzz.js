287f867bb30c18a8ca879b28b811b9c35b601cb8
Andy <neontiger88@hotmail.com>
2020-04-25 11:36:08 -0500

    Solucion del fizz buzz

---
+++ fizzBuzz.js
@@ -0,0 +1,14 @@
+let fizzBuzz = (n) => {
+    if(n % 3 == 0 && n % 5 == 0)
+        return 'FizzBuzz';
+    else if(n % 3 == 0)
+        return 'Fizz';
+    else if(n%5 == 0)
+        return 'Buzz';
+    else
+        return n;
+}
+
+for (let i = 0; i < 100; i++){
+    console.log(fizzBuzz(i))
+}
